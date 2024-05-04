b9333eee800c5ec4eb550fedc22b59ed84484ac3
Nitin <119484935+nitingit7@users.noreply.github.com>
2024-05-04 08:39:32 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,22 @@
+document.write("a: " + (42 > 3) + "<br>")
+document.write("b: " + (91 < 4) + "<br>")
+document.write("c: " + (8 == 2) + "<br>")
+document.write("d: " + (4 < 17) + "<br><hr>")
+myname = "Peter" 
+myage  = 24
+document.write("a: " + 42     + "<br>") // Numeric literal
+document.write("b: " + "Hi"   + "<br>") // String literal
+document.write("c: " + true   + "<br>") // Constant literal 
+document.write("d: " + myname + "<br>") // String variable
+document.write("e: " + myage  + "<br><hr>") // Numeric variable
+x = 6
+y = 23
+if (x<56) document.write("your's is smaller" + "<br><hr>")
+
+else      document.write("its greater then this" + "<br> <hr>")
+
+a = 3.14159
+b = "3.14159"
+if (a == b) document.write("it is different type in data types" + "<br> <hr>")
+if (a === b) document.write("equal data types" + "<br> <hr>")
+else        document.write("it is of unequal data types" + "<br> <hr>")
